bdbe4f561c2e1f810fc0234f177f88a60d64e052
Juniqua <juniquawhite@gmail.com>
2022-10-30 19:20:33 -0400

    Update wsgi.py
    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.dev")
    then gunicorn worked.
    
    tried stack overflow fix for gunicorn

--- crif/wsgi.py
+++ crif/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crif.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crif.settings.dev')
 
 application = get_wsgi_application()
