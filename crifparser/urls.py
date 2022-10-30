208d027e9e195c6d20d06786c235d2ded5d9627c
Juniqua <juniquawhite@gmail.com>
2022-10-30 19:42:52 -0400

    SETTINGS AND URLS
    CHANGED LOGOUT IN URLS AND REVERTED BACK TO ORIG SETTINGS.PY

--- crifparser/urls.py
+++ crifparser/urls.py
@@ -3,6 +3,7 @@ from crifparser import views
 from django.contrib.auth.models import User
 from django.views.generic.base import TemplateView 
 from . import views
+
 urlpatterns = [
     path("home", views.home, name="home"),
     path("info_review",views.info_review, name="info_review"),
@@ -10,5 +11,5 @@ urlpatterns = [
     path("get_zip", views.get_zip, name='download_zip'),
     path("register", views.registerpage, name="register"),
     path("",views.loginpage, name="login"),
-    path("/logout",views.logoutuser, name="logout")
+    path("logout",views.logoutuser, name="logout")
 ]
